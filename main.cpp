68f51bc49ab1c727b1de6c212766c250d8b5c9a4
Jovani <jovanidesouza@hotmail.com>
2019-09-09 19:18:37 -0300

    add camera control and some rotation

--- main.cpp
+++ main.cpp
@@ -3,53 +3,57 @@
 #include <GL/glut.h>
 #include <GL/glu.h>
 #include <math.h>
+#include <stdio.h>
 
-//variáveis para camera
-GLint eyex = 0, eyey = 0, eyez = 0;
-GLint centrox = 0, centroy = 0, centroz = 0;
+//variáveis para camera (gluLookAt)
+GLfloat eyex = 0, eyey = 0.5, eyez = 0.3;
+GLfloat centrox = 0, centroy = 0, centroz = 0.5;
+//variáveis para rotação da base
+GLfloat rotating_X = 0;
+GLfloat rotating_Y = 0;
+
+//////////////////////////////////////////////////////////////////////////////////
 
 void keyboard(int key, int x, int y){
 
-  switch(key){
-      case(GLUT_KEY_F1):{//initial position of camera 1
-        eyex = 0;
-        eyey = 0;
-        eyez = 0;
-        centrox = 0;
-        centroy = 0;
-        centroz = 0;
-        break;
-      }
-      case(GLUT_KEY_F2):{//camera 2
-        eyex = 0;
-        eyey = 0;
-        eyez = 0;
-        centrox = 0;
-        centroy = 0;
-        centroz = 0;
-        break;
-      }
+  if(key == GLUT_KEY_F1){//reset rotation
+    rotating_Y = 0;
+    rotating_X = 0;
+  } else if (key == GLUT_KEY_RIGHT) {
+		rotating_Y += 5;
+	} else if (key == GLUT_KEY_LEFT) {
+		rotating_Y -= 5;
+	} else if (key == GLUT_KEY_DOWN) {
+		rotating_X -= 5;
+	} else if (key == GLUT_KEY_UP) {
+		rotating_X += 5;
+	} else if(key == GLUT_KEY_F2){
+    eyex+=0.05;
 
-    }
+  } else if(key == GLUT_KEY_F3){
+    eyex-=0.05;
+  }
+  glutPostRedisplay();
 }
-
 
 void draw(){
 
-  //Background Color
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-  // Clear screen
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  // Reset transformations
+	glLoadIdentity();
+  gluLookAt(eyex,eyey,eyez, centrox,centroy,centroz, 0,1,0);
+	glRotatef(rotating_X, 1.0f, 0.0f, 0.0f);
+	glRotatef(rotating_Y, 0.0f, 1.0f, 0.0f);
 
   glBegin(GL_QUADS);
       //define a cor do chão
       glColor3f(0.41, 0.41, 0.41);
       //posição dos vértices do quadrado usado como parametro para localização do chão
-      glVertex3f(-0.5f, -0.5f, 0.f);
-      glVertex3f(0.5f, -0.5f, 0.f);
-      glVertex3f(0.5f, 0.5f, 0.f);
-      glVertex3f(-0.5f, 0.5f, 0.f);
-
+      glVertex3f(-0.5f, 0.f, -0.5f);
+      glVertex3f(0.5f, 0.f, -0.5f);
+      glVertex3f(0.5f, 0.f, 0.5f);
+      glVertex3f(-0.5f, 0.f, 0.5f);
   glEnd();
   glFlush();
 	glutSwapBuffers();
@@ -57,12 +61,13 @@ void draw(){
 
 
 int main(int argc, char **argv) {
+
   //is used to initialize the GLUT library.
   glutInit(&argc, argv);
-  //Bit mask to select a single(can be GLUT_DOUBLE) buffered window.//RGB//Bit mask to select a window with a depth buffer.
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+  //Bit mask to select a single(can be GLUT_SINGLE) buffered window.//RGB//Bit mask to select a window with a depth buffer.
+  glutInitDisplayMode(GLUT_DOUBLE| GLUT_RGB | GLUT_DEPTH);
   //windowsize
-  glutInitWindowSize(800, 700);
+  glutInitWindowSize(1000, 900);
   //posição da criação da janela (1,1) canto superior esquerdo.
   glutInitWindowPosition(1, 1);
   //name of the window
